b33d2f654351dd52df42d847ba60e6dc54bb02de
DiVaSkibo <d.v.skibo@student.khai.edu>
2025-08-07 14:28:46 +0200

    v 0.3 re 1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -42,7 +42,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "663327ffeef27d2fef3f459000a2b647
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "5a6b35ebbdb5dda025c00d343c87e027",
+"flutter_bootstrap.js": "49df857824be45c28d65f4e3f129845f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
